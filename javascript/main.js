85ee6287ae8b1238a7cc3330dc35801fdc44b583
boostaboosh <mail.laurencesnow@gmail.com>
2022-09-28 16:20:43 +0100

    Add getComputerChoice function
    Add function which returns a random result of either rock, paper, or
    scissors.

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,23 @@
-console.log("parmigiano");
+function getComputerChoice() {
+    //return a random result of either rock, paper, or scissors
+    //1 - simpler version: return a random result
+    //2 - simpler version: return a random result between 1 and 3
+    //3 - return rock paper or scissors depending on if random number is between 0 and 1, 1 and 2, or 2 and 3
+    let randomNumberBetweenZeroAndOne = Math.random();
+    let randomNumberBetweenZeroAndThree = randomNumberBetweenZeroAndOne * 3;
+    switch (true) {
+        case (randomNumberBetweenZeroAndThree<1):
+            return "rock";
+            break;
+        case (randomNumberBetweenZeroAndThree >= 1 && randomNumberBetweenZeroAndThree < 2):
+            return "paper";
+            break;
+        case (randomNumberBetweenZeroAndThree >= 2 && randomNumberBetweenZeroAndThree < 3):
+            return "scissors";
+            break;
+        default:
+            return("random number is >= 3");
+    }
+}
+
+console.log(getComputerChoice());
