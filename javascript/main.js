b7c91a8100fe462068670fdf66e6e39464f00341
boostaboosh <mail.laurencesnow@gmail.com>
2022-09-29 16:45:39 +0100

    Add playGame function
    Add a playGame function which plays a 5 round game that keeps score and
    reports a winner or loser at the end.

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
-    //return a random result of either rock, paper, or scissors
-    //1 - simpler version: return a random result
-    //2 - simpler version: return a random result between 1 and 3
-    //3 - return rock paper or scissors depending on if random number is between 0 and 1, 1 and 2, or 2 and 3
+    // Return a random result of either rock, paper, or scissors.
+    // 1 - simpler version: return a random result.
+    // 2 - simpler version: return a random result between 1 and 3.
+    // 3 - return rock paper or scissors depending on if random number is between 0 and 1, 1 and 2, or 2 and 3.
     let randomNumberBetweenZeroAndOne = Math.random();
     let randomNumberBetweenZeroAndThree = randomNumberBetweenZeroAndOne * 3;
     switch (true) {
@@ -21,54 +21,88 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    //return a string that declares the winner of the round of rock paper scissors and makes the playerChoice parameter case insensitive.
-    //1) simpler: return a string
-    //2) return the winner between rock and paper
-    //3) return the winner between rock, paper, and scissors
-    //4) mate the playerChoice parameter case insensitive
+    // Return a string that declares the winner of the round of rock paper scissors and makes the playerChoice parameter case insensitive.
+    // 1) Simpler: Return a string.
+    // 2) Return the winner between rock and paper.
+    // 3) Return the winner between rock, paper, and scissors.
+    // 4) Make the playerChoice parameter case insensitive.
     playerChoice = playerChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
 
-    let winner;
-    
+    let roundResult;
+
     if (playerChoice === "rock") {
         switch (true) {
             case computerChoice === "paper":
-                winner = "Looooooooooser.";
+                roundResult = "loss";
                 break;
             case computerChoice === "rock":
-                winner = "La mujer bebe la leche. You draw."
+                roundResult = "draw";
                 break;
             case computerChoice === "scissors":
-                winner = "flawless victory.";
+                roundResult = "win";
                 break;
             default:
-                winner = "Unknown winner.";
+                roundResult = "unknown";
         }
     } else if (playerChoice === "paper") {
         switch (true) {
             case computerChoice === "rock":
-                winner = "Well done monkey brain, you win.";
+                roundResult = "win";
                 break;
             case computerChoice === "paper":
-                winner = "No one wins. You draw hombre.";
+                roundResult = "draw";
                 break;
             case computerChoice === "scissors":
-                winner = "Ha ha, you lose.";
+                roundResult = "loss";
                 break;
             default:
-                winner = "No one wins. You all die.";
+                roundResult = "unknown";
         }
     } else if (playerChoice === "scissors") {
         if (computerChoice === "rock") {
-            winner = "You lose therefore you suck.";
+            roundResult = "loss";
         } else if (computerChoice === "paper"){
-            winner = "History is written by the victors, and you won.";
+            roundResult = "win";
         } else if (computerChoice === "scissors"){
-            winner = "There are no winners, only losers. You draw";
+            roundResult = "draw";
         }
     }
 
-    return winner;
+    return roundResult;
 }
 
+let game = function() {
+    // Play a 5 round game that keeps score and reports a winner or loser at the end.
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for (games = 0; games < 5; games ++) {
+        let userChoice = prompt("Rock, Paper, or Scissors?");
+        let result = playRound(userChoice, getComputerChoice());
+        if (result === "win") {
+            playerWins ++;
+            console.log("You win this battle, but you will lose this war.");
+        } else if (result === "loss") {
+            computerWins ++;
+            console.log("You lose therefore you suck.");
+        } else if (result === "draw") {
+            console.log("Draw. No one wins. You all die.");
+        }
+    }
+
+    outputResult();
+}
+
+outputResult = function(playerWins, computerWins) {
+    // Calculate who won the game and output the result.
+    if (playerWins > computerWins) {
+        console.log("Human wins! You won the game.");
+    } else if (computerWins > playerWins) {
+        console.log("Computer wins! You lost the game.");
+    } else if (computerWins === playerWins){
+        console.log("Humans and Computers tie! You drew the game.");
+    }
+}
+
+game();
